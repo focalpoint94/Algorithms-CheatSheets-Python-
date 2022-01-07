357a454b7a0703cef4105378fc9016ccae5ec62b
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-07 11:09:19 +0900

    Update AVL.py

--- AVL.py
+++ AVL.py
@@ -1,129 +1,146 @@
-class node:
-    def __init__(self, num):
-        self.value = num
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
         self.height = 1
 
 class AVL:
-    def height(self, Node):
-        if Node is None:
+    def __init__(self):
+        self.root = None
+
+    def _height(self, node):
+        if not node:
             return 0
-        else:
-            return Node.height
+        return node.height
 
-    def balance(self, Node):
-        if Node is None:
+    def _balance(self, node):
+        if not node:
             return 0
+        return self._height(node.left) - self._height(node.right)
+
+    def _find_successor_node(self, node):
+        while node.left:
+            node = node.left
+        return node
+
+    def _rotateR(self, node):
+        y = node.left
+        t2 = y.right
+        y.right = node
+        node.left = t2
+        node.height = 1 + max(self._height(node.left), self._height(node.right))
+        y.height = 1 + max(self._height(y.left), self._height(y.right))
+        return y
+
+    def _rotateL(self, node):
+        y = node.right
+        t2 = y.left
+        y.left = node
+        node.right = t2
+        node.height = 1 + max(self._height(node.left), self._height(node.right))
+        y.height = 1 + max(self._height(y.left), self._height(y.right))
+        return y
+
+    def insert(self, val):
+        self.root = self._insert(self.root, val)
+
+    def _insert(self, node, val):
+        # Termination: Reaching Leaf
+        if not node:
+            return TreeNode(val)
+
+        if val < node.val:
+            node.left = self._insert(node.left, val)
         else:
-            return self.height(Node.left) - self.height(Node.right)
+            node.right = self._insert(node.right, val)
+        node.height = 1 + max(self._height(node.left), self._height(node.right))
 
-    def MinimumValueNode(self, Node):
-        if Node is None or Node.left is None:
-            return Node
+        balance = self._balance(node)
+        # LL Case -> Right Rotation of node
+        if balance > 1 and val < node.left.val:
+            return self._rotateR(node)
+        # RR Case -> Left Rotation of node
+        if balance < -1 and val > node.right.val:
+            return self._rotateL(node)
+        # LR Case -> Left Rotation of node.left -> Right Rotation of node
+        if balance > 1 and val > node.left.val:
+            node.left = self._rotateL(node.left)
+            return self._rotateR(node)
+        # RL Case -> Right Rotation of node.right -> Left Rotation of node
+        if balance < -1 and val < node.right.val:
+            node.right = self._rotateR(node.right)
+            return self._rotateL(node)
+        return node
+
+    def delete(self, val):
+        self.root = self._delete(self.root, val)
+
+    def _delete(self, node, val):
+        if not node:
+            return None
+        if val < node.val:
+            node.left = self._delete(node.left, val)
+        elif val > node.val:
+            node.right = self._delete(node.right, val)
+        # Node Found
         else:
-            return self.MinimumValueNode(Node.left)
+            # If node has only one child
+            if not node.right:
+                return node.left
+            if not node.left:
+                return node.right
+            # If node has two children
+            successor_node = self._find_successor_node(node.right)
+            node.val = successor_node.val
+            node.right = self._delete(node.right, successor_node.val)
+        # If only None is left
+        if not node:
+            return None
 
-    def rotateR(self, Node):
-        a = Node.left
-        b = a.right
-        a.right = Node
-        Node.left = b
-        Node.height = 1 + max(self.height(Node.left), self.height(Node.right))
-        a.height = 1 + max(self.height(a.left), self.height(a.right))
-        return a
+        node.height = 1 + max(self._height(node.left), self._height(node.right))
+        balance = self._balance(node)
+        # LL Case
+        if balance > 1 and self._balance(node.left) >= 0:
+            return self._rotateR(node)
+        # RR Case
+        if balance < -1 and self._balance(node.right) <= 0:
+            return self._rotateL(node)
+        # LR Case
+        if balance > 1 and self._balance(node.left) < 0:
+            node.left = self._rotateL(node.left)
+            return self._rotateR(node)
+        # RL Case
+        if balance < -1 and self._balance(node.right) > 0:
+            node.right = self._rotateR(node.right)
+            return self._rotateL(node)
+        return node
 
-    def rotateL(self, Node):
-        a = Node.right
-        b = a.left
-        a.left = Node
-        Node.right = b
-        Node.height = 1 + max(self.height(Node.left), self.height(Node.right))
-        a.height = 1 + max(self.height(a.left), self.height(a.right))
-        return a
+    def preorder(self):
+        self._preorder(self.root)
 
-    def insert(self, val, root):
-        if root is None:
-            return node(val)
-        elif val <= root.value:
-            root.left = self.insert(val, root.left)
-        elif val > root.value:
-            root.right = self.insert(val, root.right)
-        root.height = 1 + max(self.height(root.left), self.height(root.right))
-        balance = self.balance(root)
-        if balance > 1 and root.left.value > val:
-            return self.rotateR(root)
-        if balance < -1 and val > root.right.value:
-            return self.rotateL(root)
-        if balance > 1 and val > root.left.value:
-            root.left = self.rotateL(root.left)
-            return self.rotateR(root)
-        if balance < -1 and val < root.right.value:
-            root.right = self.rotateR(root.right)
-            return self.rotateL(root)
-        return root
-
-    def preorder(self, root):
-        if root is None:
+    def _preorder(self, node):
+        if not node:
             return
-        print(root.value)
-        self.preorder(root.left)
-        self.preorder(root.right)
-
-    def delete(self, val, Node):
-        if Node is None:
-            return Node
-        elif val < Node.value:
-            Node.left = self.delete(val, Node.left)
-        elif val > Node.value:
-            Node.right = self.delete(val, Node.right)
-        else:
-            if Node.left is None:
-                lt = Node.right
-                Node = None
-                return lt
-            elif Node.right is None:
-                lt = Node.left
-                Node = None
-                return lt
-            rgt = self.MinimumValueNode(Node.right)
-            Node.value = rgt.value
-            Node.right = self.delete(rgt.value, Node.right)
-        if Node is None:
-            return Node
-        Node.height = 1 + max(self.height(Node.left), self.height(Node.right))
-        balance = self.balance(Node)
-        if balance > 1 and self.balance(Node.left) >= 0:
-            return self.rotateR(Node)
-        if balance < -1 and self.balance(Node.right) <= 0:
-            return self.rotateL(Node)
-        if balance > 1 and self.balance(Node.left) < 0:
-            Node.left = self.rotateL(Node.left)
-            return self.rotateR(Node)
-        if balance < -1 and self.balance(Node.right) > 0:
-            Node.right = self.rotateR(Node.right)
-            return self.rotateL(Node)
-        return Node
+        print(node.val, end=' ')
+        print('down left: ', end=' ')
+        self._preorder(node.left)
+        print('returned;', end=' ')
+        print('down right: ', end=' ')
+        self._preorder(node.right)
+        print('returned;', end=' ')
 
 
-Tree = AVL()
-rt = None
-rt = Tree.insert(3, rt)
-rt = Tree.insert(5, rt)
-rt = Tree.insert(7, rt)
-print("PREORDER")
-Tree.preorder(rt)
-rt = Tree.insert(1, rt)
-rt = Tree.insert(2, rt)
-print("PREORDER")
-Tree.preorder(rt)
-rt = Tree.insert(4, rt)
-rt = Tree.insert(6, rt)
-rt = Tree.delete(7, rt)
-rt = Tree.insert(8, rt)
-rt = Tree.insert(9, rt)
-print("PREORDER")
-Tree.preorder(rt)
-rt = Tree.delete(3, rt)
-print("PREORDER")
-Tree.preorder(rt)
+
+
+tree = AVL()
+tree.insert(3)
+tree.insert(4)
+tree.insert(1)
+tree.insert(2)
+tree.insert(9)
+tree.insert(17)
+tree.insert(22)
+tree.insert(-3)
+tree.delete(3)
+tree.preorder()
