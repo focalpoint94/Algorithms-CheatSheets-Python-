d3742ae3f41cc99687b2f4a8164bf3a10e9af67c
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-21 11:59:06 +0900

    Update Trie.py

--- Trie.py
+++ Trie.py
@@ -3,11 +3,11 @@ class TrieNode:
         self.children = {}
         self.endFlag = False
 
-        
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        
+
     def insert(self, word: str) -> None:
         node = self.root
         for char in word:
@@ -23,7 +23,7 @@ class Trie:
                 return False
             node = node.children[char]
         return node.endFlag
-        
+
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for char in prefix:
@@ -31,22 +31,23 @@ class Trie:
                 return False
             node = node.children[char]
         return True
-    
+
     def delete(self, word):
-        def recursive(node, word, i):
-            if i == len(word):
-                if not node.endFlag:
-                    return False
-                node.endFlag = False
-                return (len(node.children) == 0)
-            if word[i] not in node.children:
+        return self._delete(self.root, word, 0)
+
+    def _delete(self, node, word, i):
+        if i == len(word):
+            if not node.endFlag:
                 return False
-            need_delete = recursive(node.children[word[i]], word, i+1)
-            if need_delete:
-                node.children.pop(word[i])
-                return (len(node.children) == 0)
+            node.endFlag = False
+            return len(node.children) == 0
+        if word[i] not in node.children:
             return False
-        recursive(self.root, word, 0)
+        need_delete = self._delete(node.children[word[i]], word, i+1)
+        if need_delete:
+            node.children.pop(word[i])
+            return len(node.children) == 0
+        return False
         
         
 
