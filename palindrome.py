7c81790e9acd9982e2241f0727900aca1dc1baad
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-15 11:14:04 +0900

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,8 @@
-class Solution:
-    def build_palindrome(self, word):
+    def palindrome(self, word):
         n = len(word)
         table = [[False] * n for _ in range(n)]
         for j in range(n):
             for i in range(j+1):
-                if s[j] == s[i] and (j - i <= 2 or table[i+1][j-1]):
+                if word[j] == word[i] and (j - i <= 2 or table[i+1][j-1]):
                     table[i][j] = True
         return table
-        
-
-    
