9a9d48941bb80b6c287f6a9061fc031915c8d32d
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-15 11:00:55 +0900

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,12 @@
 class Solution:
     def build_palindrome(self, word):
         n = len(word)
-        dp = [[False] * n for _ in range(n)]
-        for end in range(n):
-            for start in range(end+1):
-                if s[end] == s[start] and (end - start <= 2 or dp[start+1][end-1]):
-                    dp[start][end] = True
-        return dp
+        table = [[False] * n for _ in range(n)]
+        for j in range(n):
+            for i in range(j+1):
+                if s[j] == s[i] and (j - i <= 2 or table[i+1][j-1]):
+                    table[i][j] = True
+        return table
         
 
     
