10d749e46065bc8029fe53365d23ac1f1a343da5
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-03-23 09:57:17 +0900

    Update heap_with_lazy_remove.py

--- heap_with_lazy_remove.py
+++ heap_with_lazy_remove.py
@@ -1,24 +1,50 @@
 import heapq
 
-class PriorityQueue:
-    def __init__(self):
-        self.pq = []
+class MyHeap:
+    def __init__(self, type='min'):
+        self.heap = []
         self.removals = []
+        self.sign = 1 if type == 'min' else -1
 
     def insert(self, elem):
-        heapq.heappush(self.pq, -elem)
+        heapq.heappush(self.heap, self.sign * elem)
 
-    def gettop(self):
-        return -self.pq[0]
+    def __getitem__(self, idx):
+        return self.sign * self.heap[idx]
+
+    def __len__(self):
+        return len(self.heap) - len(self.removals)
 
     def remove(self, elem):
-        if elem == -self.pq[0]:
-            heapq.heappop(self.pq)
-            while self.removals and self.pq[0] == self.removals[0]:
-                heapq.heappop(self.pq)
+        if elem == self.sign * self.heap[0]:
+            heapq.heappop(self.heap)
+            while self.removals and self.heap[0] == self.removals[0]:
+                heapq.heappop(self.heap)
                 heapq.heappop(self.removals)
         else:
-            heapq.heappush(self.removals, -elem)
+            heapq.heappush(self.removals, self.sign * elem)
 
-    def length(self):
-        return len(self.pq)
+# import heapq
+
+# class PriorityQueue:
+#     def __init__(self):
+#         self.pq = []
+#         self.removals = []
+
+#     def insert(self, elem):
+#         heapq.heappush(self.pq, -elem)
+
+#     def gettop(self):
+#         return -self.pq[0]
+
+#     def remove(self, elem):
+#         if elem == -self.pq[0]:
+#             heapq.heappop(self.pq)
+#             while self.removals and self.pq[0] == self.removals[0]:
+#                 heapq.heappop(self.pq)
+#                 heapq.heappop(self.removals)
+#         else:
+#             heapq.heappush(self.removals, -elem)
+
+#     def length(self):
+#         return len(self.pq)
