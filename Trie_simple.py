9ea92a54b42db4308d1145ba69ce608c40f301d5
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-13 08:35:28 +0900

    Update Trie_simple.py

--- Trie_simple.py
+++ Trie_simple.py
@@ -1,8 +1,35 @@
-trie = {}
-for word in words:
-  t = trie
-  for char in word:
-    if char not in t:
-      t[char] = {}
-    t = t[char]
-  t['#'] = '#'
+class Trie:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['#'] = '#'
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return '#' in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
